222232a7cdc35841cd25dfa103a2469e728738f6
Anthony Hom <anthonyhom2@hotmail.com>
2017-12-12 11:02:11 -0500

    Started

---
+++ pipe_networking.c
@@ -0,0 +1,35 @@
+#include "pipe_networking.h"
+
+
+/*=========================
+  server_handshake
+  args: int * to_client
+
+  Perofrms the client side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe.
+
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_handshake(int *to_client) {
+  int wkp = mkfifo("WKP", 0644);
+	int fd = open("WKP", O_RDONLY);
+	
+  return 0;
+}
+
+
+/*=========================
+  client_handshake
+  args: int * to_server
+
+  Perofrms the client side pipe 3 way handshake.
+  Sets *to_server to the file descriptor for the upstream pipe.
+
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int client_handshake(int *to_server) {
+	int fd = open("WKP", O_WRONLY);
+	char *buf = "";	
+	write(fd, )  
+	return 0;
+}
